3b93b6cddaa138abf16b01f76868607bb750f6bf
Michael Papasevastos <michael.papasevastos@microsoft.com>
2020-01-28 09:56:08 -0600

    misc changes

--- src/generateLoad/driver.ps1
+++ src/generateLoad/driver.ps1
@@ -1,4 +1,5 @@
-# quick test
-# artillery quick --count 10 -n 20 https://artillery.io/
-
+# go to https://artillery.io for info on the tool
+# quick test to check if tool is installed correctly and working:
+#   artillery quick --count 10 -n 20 https://artillery.io/
 artillery run load.yml
+
